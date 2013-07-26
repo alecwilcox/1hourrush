bb8027f8273c10f1f1521976db934acd20e488d9
MrFord <kylejford@me.com>
2013-07-26 16:03:20 -0400

    Upload

---
+++ 1 Hour Rush/1 Hour Rush/FlipsideViewController.m
@@ -0,0 +1,41 @@
+//
+//  FlipsideViewController.m
+//  1 Hour Rush
+//
+//  Created by Kyle Ford on 7/26/13.
+//  Copyright (c) 2013 TinkerSoft. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface FlipsideViewController ()
+
+@end
+
+@implementation FlipsideViewController
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)done:(id)sender
+{
+    [self.delegate flipsideViewControllerDidFinish:self];
+}
+
+@end
