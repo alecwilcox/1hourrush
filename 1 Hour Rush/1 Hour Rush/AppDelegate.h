0dc724b366df45fc1e1dcd1d1851bd7e1db169fb
Alec Wilcox <alec.wilcox@me.com>
2013-07-26 16:13:57 -0400

    New Project

--- 1 Hour Rush/1 Hour Rush/AppDelegate.h
+++ 1 Hour Rush/1 Hour Rush/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  1 Hour Rush
 //
-//  Created by Kyle Ford on 7/26/13.
+//  Created by Alec Wilcox  on 7/26/13.
 //  Copyright (c) 2013 TinkerSoft. All rights reserved.
 //
 
@@ -11,12 +11,5 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
